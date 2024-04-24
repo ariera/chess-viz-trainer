c4f8bfa5b8cff96e57350dd94f490ac64c84b343
Alejandro Riera <alejandro.riera@embo.org>
2024-04-24 20:32:19 +0200

    add images for both exercises

--- src/views/home.vue
+++ src/views/home.vue
@@ -5,6 +5,23 @@ import KnightMoves from '../knight-moves/index.vue'
 </script>
 
 <template lang="pug">
-o-button(@click="$router.push('/knight-moves')") Knight Moves
-o-button(@click="$router.push('/checks-n-captures')") Checks & Captures
+.columns
+  .column
+    .card.is-clickable(@click="$router.push('/knight-moves')")
+      .card-image
+        figure.image
+          img(src='@/assets/knight-moves.webp' alt='Knight Moves')
+      .card-content
+        p.title.is-4 Knight moves
+        p.subtitle.is-6
+
+  .column
+    .card.is-clickable(@click="$router.push('/checks-n-captures')")
+      .card-image
+        figure.image
+          img(src='@/assets/checks-n-captures.webp' alt='Checks and Captures')
+      .card-content
+        p.title.is-4 Checks & Captures
+        p.subtitle.is-6
+
 </template>
