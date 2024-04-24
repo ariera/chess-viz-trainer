3d1836b581785154fa4c8142d471e4c7be88ae5f
Alejandro Riera <alejandro.riera@embo.org>
2024-04-24 21:07:38 +0200

    add updated bulma with light theme only and use grid for the home view

--- src/views/home.vue
+++ src/views/home.vue
@@ -5,23 +5,37 @@ import KnightMoves from '../knight-moves/index.vue'
 </script>
 
 <template lang="pug">
-.columns
-  .column
-    .card.is-clickable(@click="$router.push('/knight-moves')")
-      .card-image
-        figure.image
-          img(src='@/assets/knight-moves.webp' alt='Knight Moves')
-      .card-content
-        p.title.is-4 Knight moves
-        p.subtitle.is-6
+.fixed-grid.has-auto-count
+  .grid
+    .cell
+      .card.is-clickable(@click="$router.push('/knight-moves')")
+        .card-image
+          figure.image
+            img(src='@/assets/knight-moves.webp' alt='Knight Moves')
+        .card-content
+          p.title.is-4 Knight Moves
+          p.subtitle.is-6
 
-  .column
-    .card.is-clickable(@click="$router.push('/checks-n-captures')")
-      .card-image
-        figure.image
-          img(src='@/assets/checks-n-captures.webp' alt='Checks and Captures')
-      .card-content
-        p.title.is-4 Checks & Captures
-        p.subtitle.is-6
+    .cell
+      .card.is-clickable(@click="$router.push('/checks-n-captures')")
+        .card-image
+          figure.image
+            img(src='@/assets/checks-n-captures.webp' alt='Checks and Captures')
+        .card-content
+          p.title.is-4 Checks & Captures
+          p.subtitle.is-6
 
 </template>
+
+<style scoped>
+
+.card {
+  max-width: 25rem;
+}
+
+@media screen and (max-width: 768px) {
+  .card {
+    max-width: none;
+  }
+}
+</style>
